2fe107d78ed714d5247ee69ae5daa582bc70284c
Paul Antoni <paul.antoni@siemens-healthineers.com>
2023-05-10 18:33:52 +0200

    feat: add Agent, AgentController and Move classes

---
+++ CityPlanner/CityPlanner/Move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityPlanner
+{
+    class Move
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Move(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
