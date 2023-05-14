27865de7019b8faa8bc15fbbe58e21cbd1cf0b7f
Leo Schnüll <leo.schnuell@gmx.de>
2023-05-14 23:22:03 +0200

    Feature: Added Settings page and Navigationroot

--- CityPlanner/CityPlannerFrontend/App.xaml.cs
+++ CityPlanner/CityPlannerFrontend/App.xaml.cs
@@ -45,9 +45,25 @@ namespace CityPlannerFrontend
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+
+            // Create a Frame to act as the navigation context and navigate to the first page
+            Frame rootFrame = new Frame();
+            rootFrame.NavigationFailed += OnNavigationFailed;
+            // Navigate to the first page, configuring the new page
+            // by passing required information as a navigation parameter
+            rootFrame.Navigate(typeof(MainPage), args.Arguments);
+
+            // Place the frame in the current Window
+            m_window.Content = rootFrame;
+            // Ensure the MainWindow is active
             m_window.Activate();
+            
         }
 
         private Window m_window;
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
     }
 }
