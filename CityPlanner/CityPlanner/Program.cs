f4a0b63f46d21dd54c7e77d10cca105a2c0dd8c1
Sander <sander.stella00@gmail.com>
2023-05-20 00:40:06 +0200

    added test.cs to be used for test later

--- CityPlanner/CityPlanner/Program.cs
+++ CityPlanner/CityPlanner/Program.cs
@@ -7,12 +7,4 @@ AppController appController = new AppController();
 
 appController.Start();
 
-/*
-GridElement n1 = new GridElement();
-Housing haus = new Housing(n1);
-haus.AddDependency(Data.GridType.Street,1);
-GridElement n2 = n1.Clone();
-Industry industry = new Industry(n2);
-Console.WriteLine(industry);
-*/
 
---
+++ CityPlanner/CityPlanner/test.cs
@@ -0,0 +1,25 @@
+﻿using CityPlanner.Grid;
+
+
+namespace CityPlanner;
+
+using CityPlanner;
+
+public class test
+{
+    bool tsetCopyofGrid_Deep()
+    {
+        GridElement n1 = new GridElement();
+        Housing haus = new Housing(n1);
+        haus.AddDependency(Data.GridType.Street,1);
+        GridElement n2 = n1.Clone();
+        Industry industry = new Industry(n2);
+
+        
+       // haus.Dependency[Data.GridType.Street].Count==1;
+       // industry.Dependency[Data.GridType.Street].Count==0;
+
+        return true;
+
+    }
+}
