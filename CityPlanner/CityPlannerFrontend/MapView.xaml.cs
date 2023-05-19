ab0e290c4c5889e571ba99cb270e526b88788e23
Leo Schnüll <leo.schnuell@gmx.de>
2023-05-19 14:47:54 +0200

    Feat: Added Mapview

--- CityPlanner/CityPlannerFrontend/MapView.xaml.cs
+++ CityPlanner/CityPlannerFrontend/MapView.xaml.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -13,10 +15,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Windows.Devices.Enumeration;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,22 +24,18 @@ namespace CityPlannerFrontend
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Settings : Page
+    public sealed partial class MapView : Page
     {
-        public int iEinwohnerzahl ;
-        public Settings()
+        public int satisfaction = 0; 
+        public int buildinglevel = 0;
+        public int rastercount = 0;
+        public MapView()
         {
             this.InitializeComponent();
-            
         }
         private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(MainPage));
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(MapView));
-            Debug.WriteLine(iEinwohnerzahl);
+            Frame.Navigate(typeof(Settings));
         }
     }
 }
