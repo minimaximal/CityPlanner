11ca3aec9c2dbd11660ccf989ced8ff0544483fe
uzslo <kevinkern0606@gmail.com>
2023-05-31 23:01:35 +0200

    added Test for Deepclone of Map

--- CityPlanner/CityPlanner/Tests.cs
+++ CityPlanner/CityPlanner/Tests.cs
@@ -4,13 +4,12 @@ using NUnit.Framework;
 namespace CityPlanner{
     
     public class Tests
-    {
+    { 
+        //Test to make sure that Grids are not cloned by reference -> Deep clone instead
         [Test]
         public void CopyofGrid_Deep()
         {
             GridElement n1 = new GridElement();
-            Housing haus = new Housing(n1);
-            haus.AddDependency(Data.GridType.Street, 1);
             GridElement n2 = n1.Clone();
             Industry industry = new Industry(n2);
             GridElement industry2 = industry.Clone();
@@ -18,7 +17,34 @@ namespace CityPlanner{
             Assert.AreEqual(industry.GetGridType(), industry2.GetGridType());
             Assert.AreNotSame(industry, industry2);
         }
+        
+        //Test to make sure that Maps are not cloned by reference -> Deep clone instead
+        [Test]
+        public void CopyofMap_Deep()
+        {
+            Map newMap = new Map(50, 50, 5000);
+            Move nextMove = new Move(49, 49);
+            nextMove.GridType = Data.GridType.Street;
+            newMap.AddMove(nextMove);
+            nextMove = new Move(49,48);
+            nextMove.GridType = Data.GridType.Commercial;
+            newMap.AddMove(nextMove);
+            Map clonedMap = (Map) newMap.Clone();
 
+            for(int i = 0; i < 50; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    Assert.AreEqual(newMap.GetGridElement(i,j)!.GetGridType(), clonedMap.GetGridElement(i,j)!.GetGridType());
+                    Assert.AreEqual(newMap.GetGridElement(i,j)!.GetLevel(), clonedMap.GetGridElement(i,j)!.GetLevel());
+                    Assert.AreNotSame(newMap.GetGridElement(i,j), clonedMap.GetGridElement(i,j));
+                }
+            }
+            Assert.AreNotSame(newMap, clonedMap);
+        }
+        
+        
+        //Test for sorting moves according to position on Map
         [Test]
         public void MoveSort()
         {
