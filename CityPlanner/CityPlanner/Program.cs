7fc922b4eb0cc5f917d23e9497a6fcb8198e0060
Sander <sander.stella00@gmail.com>
2023-06-03 20:51:44 +0200

    fixed progream ending to fast

--- CityPlanner/CityPlanner/Program.cs
+++ CityPlanner/CityPlanner/Program.cs
@@ -11,15 +11,16 @@
 
 using CityPlanner;
 
-AppController appController = new AppController(10000, 50, 20 , 10);
-
-for (int j = 0; j < 10000; j++)
+AppController appController = new AppController(100000, 50, 20 , 10);
+Map map;
+for (int j = 0; j < 1000000000; j++)
 {
-    if(j%10 !=0 ) continue;
+    map =  appController.nextGeneration();
+
+    if(j%100 !=0 ) continue;
+    
     Console.WriteLine("gen:" + j );
-    Map  map = appController.nextGeneration();
-    Console.WriteLine(map.getScore());
-    Console.WriteLine(map.GetPeople());
+
     map.NewDisplay();
 }
 
