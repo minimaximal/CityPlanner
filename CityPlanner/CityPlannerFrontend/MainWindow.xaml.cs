3cb085adfbdbd65063659855e1d7a2da1a7fc96b
minimaximal <maximilian.koch@hk.mailbox.de>
2023-06-08 15:39:14 +0200

    readded / fixed: titlebar

--- CityPlanner/CityPlannerFrontend/MainWindow.xaml.cs
+++ CityPlanner/CityPlannerFrontend/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.UI.Xaml;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,11 +20,21 @@ namespace CityPlannerFrontend
         public MainWindow()
         {
             this.InitializeComponent();
+
+            var mAppWindow = GetAppWindowForCurrentWindow();
+            mAppWindow.Title = "City Planner";
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void MyButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            var hWnd = WindowNative.GetWindowHandle(this);
+            var wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
     }
 }
