5cb998a52428c3826ae9102b40da0fdbe9097829
minimaximal <maximilian.koch@hk.mailbox.de>
2023-06-10 09:58:28 +0200

    refactor Data.cs (alphabetic order and removed unsued var)

--- CityPlanner/CityPlanner/Data.cs
+++ CityPlanner/CityPlanner/Data.cs
@@ -7,37 +7,33 @@ public static class Data
    public static int ImportQuota;
    public static int OptimalIndustryAmount;
    public static List<(int, int)> InitialStreets = null!;
-    public static double MutationChance;
+   public static double MutationChance;
 
 
    // Empty must be the last entry in this list otherwise Agent.cs:getRandomMove() does not work
    public enum GridType
    {
-      Housing,
-      Commercial,
-      Industry,
-      Subway,
-      Sight,
-      Street,
-      Blocked,
-      Highway,
-      Empty
-
+       Blocked,
+       Commercial,
+       Highway,
+       Housing,
+       Industry,
+       Sight,
+       Street,
+       Subway,
+       Empty
    };
-
-   // Holds the amount of grid types excluding Empty
-   public static readonly int GridTypeAmount = Enum.GetValues(typeof(GridType)).Length - 1;
 
    public static readonly Dictionary<GridType, double> GridTypeMaxRange = new()
     {
-        { GridType.Housing, 5 },
+        { GridType.Blocked, 0 },
         { GridType.Commercial, 6.5 },
+        { GridType.Highway, 6 },
+        { GridType.Housing, 5 },
         { GridType.Industry, 4.9 },
-        { GridType.Subway, 5.0 },
         { GridType.Sight, 6.0 },
         { GridType.Street, 3.5 },
-        { GridType.Blocked, 0 },
-        { GridType.Highway, 6 },
+        { GridType.Subway, 5.0 },
         { GridType.Empty, 0 }
     };
 
